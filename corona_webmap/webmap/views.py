116536cce2ede1a3582a5e6b4b8c0b3070988eb9
Stchena <szczesny.simon@gmail.com>
2020-09-07 16:00:04 +0200

    Adding basic homepage Map from OpenLayers JS
    Summary:
    - JS files copied from https://github.com/GeriLife/coronavirus_mapper/tree/master/coronavirus_mapper/reports/static/js/reports
    - Will need to modify Map layer and Vector layer to enable country
    selection and viewing corona data from our db

--- corona_webmap/webmap/views.py
+++ corona_webmap/webmap/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class WebmapTemplateView(TemplateView):
+    template_name = "webmap/map.html"
