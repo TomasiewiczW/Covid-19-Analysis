7ae86de09117c7288e3e5a1e37b4206b344fd048
adrian14857 <adrianmoskwik97@gmail.com>
2020-09-09 22:58:47 +0200

    admin.py

--- corona_webmap/webmap/admin.py
+++ corona_webmap/webmap/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Country
+from .models import WorldBorder
 
 
-@admin.register(Country)
-class CountryAdmin(OSMGeoAdmin):
+@admin.register(WorldBorder)
+class WorldBorderAdmin(OSMGeoAdmin):
     default_lon = 0  # underscores for readability
     default_lat = 0
     default_zoom = 1
