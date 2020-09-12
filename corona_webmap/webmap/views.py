ff2dffe3bb449629a8571b0431160e4dbdf5d1f4
adrianmo14857 <57635925+adrianmo14857@users.noreply.github.com>
2020-09-12 10:55:55 +0200

    Load data to country, by url address
    Send data on api/<str:pk>/ , so for example api/Poland

--- corona_webmap/webmap/views.py
+++ corona_webmap/webmap/views.py
@@ -8,6 +8,8 @@ from webmap import models
 from .serializers import CountrySerializer, SelectedGeometrySerializer
 from webmap import models
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 class CountryViewSet(ReadOnlyModelViewSet):
@@ -29,3 +31,77 @@ class SelectedGeometryViewSet(ReadOnlyModelViewSet):
 
 class MainPageView(TemplateView):
     template_name = "webmap/map.html"
+
+    
+@api_view(['GET'])
+def dataLoad(request,pk):
+
+
+    #main class
+    try:
+        country = models.WorldBorder.objects.get(name=pk)
+    except models.WorldBorder.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    #supplemental data
+    gdp = models.GDP.objects.get(country_id=country.name)
+    smoking = models.Smoking.objects.get(country_id=country.name)
+    healthcare = models.Healthcare.objects.get(country_id=country.name)
+    bloodtype = models.Bloodtype.objects.get(country_id=country.name)
+
+    #covid data
+    confirmed = models.Confirmed.objects.filter(country_id=country.name).latest('date')
+    recovered = models.Recovered.objects.filter(country_id=country.name).latest('date')
+    deaths = models.Deaths.objects.filter(country_id=country.name).latest('date')
+
+
+    all_data = {
+        'Country':
+            {
+                'name': country.name,
+                'area': country.area,
+                'pop2005':country.pop2005,
+                'fips': country.fips,
+                'iso2': country.iso2,
+                'iso3': country.iso3,
+                'un': country.un,
+                'region': country.region,
+                'subregion': country.subregion,
+                'lon':country.lon,
+                'lat': country.lat,
+            },
+        'Date': confirmed.date,
+        'Confirmed_total': confirmed.total,
+        'Recovered_total': recovered.total,
+        'Deaths_total': deaths.total,
+        'Bloodtype':
+            {
+                'Ominus': bloodtype.Ominus,
+                'Oplus': bloodtype.Oplus,
+                'Aminus': bloodtype.Aminus,
+                'Aplus': bloodtype.Aplus,
+                'Bminus': bloodtype.Bminus,
+                'Bplus': bloodtype.Bplus,
+                'ABminus': bloodtype.ABminus,
+                'ABplus': bloodtype.ABplus
+            },
+        'Healthcare':
+            {
+                'rank': healthcare.rank,
+                'score': healthcare.score
+            },
+        'Smoking':
+            {
+                'male': smoking.male,
+                'female': smoking.female,
+                'total': smoking.total
+            },
+        'GDP':
+            {
+                'rank': gdp.rank,
+                'gdpPerCapita': gdp.gdpPerCapita
+            }
+    }
+
+    return JsonResponse(all_data)
